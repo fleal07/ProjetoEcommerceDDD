34917de600f99296d34361faa61247b53f463b7e
fleal07 <fleal07@gmail.com>
2020-08-09 07:16:41 -0300

    Ajuste nas classes das Entidades

---
+++ Entities/Entities/Enums/TipoUsuario.cs
@@ -0,0 +1,8 @@
+namespace Entities.Entities.Enums
+{
+    public enum TipoUsuario
+    {
+        Administrador = 1,
+        Comum = 2
+    }
+}
--- Entities/Entities/Product.cs
+++ Entities/Entities/Product.cs
@@ -1,30 +1,56 @@
 ﻿using Entities.Notifications;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Entities.Entities
 {
     [Table("Product")]
-    public class Product: Notifies
+    public class Product : Notifies
     {
         [Column("PRD_ID")]
-        [Display(Name ="Código")]
+        [Display(Name = "Código")]
         public int Id { get; set; }
 
         [Column("PRD_NOME")]
+        [MaxLength(255)]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
+
+        [Column("PRD_DESCRICAO")]
+        [MaxLength(150)]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Column("PRD_OBSERVACAO")]
+        [MaxLength(20000)]
+        [Display(Name = "Observação")]
+        public string Observacao { get; set; }
 
         [Column("PRD_VALOR")]
         [Display(Name = "Valor")]
         public decimal Valor { get; set; }
 
+        [Column("PRD_QTD_ESTOQUE")]
+        [Display(Name = "Quantidade Estoque")]
+        public int QtdEstoque { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
         [Column("PRD_ESTADO")]
         [Display(Name = "Estado")]
         public bool Estado { get; set; }
 
+        [Column("PRD_DATA_CADASTRO")]
+        [Display(Name = "Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("PRD_DATA_ALTERACAO")]
+        [Display(Name = "Data de Alteração")]
+        public DateTime DataAlteracao { get; set; }
     }
 }
